2b6ed04b436d04637807b316b88524ddb49c919c
Jeff Sharp <jeff.sharp@git>
2019-01-30 08:43:16 -0800

    Clean up a bit

--- profile.ps1
+++ profile.ps1
@@ -1,21 +1,25 @@
-if ([Environment]::OSVersion.Version.Major -lt 10) {
-    Import-Module PsGet
-}
-
+# Enable bash-style completion
 if ($host.Name -eq 'ConsoleHost') {
     Import-Module PSReadline
     Set-PSReadLineKeyHandler -Key Tab -Function Complete
 }
 
-$LocalProfile = "$PSScriptRoot\Local_profile.ps1"
+# Load the local profile
+$LocalProfile = Join-Path $PSScriptRoot Local_profile.ps1
 if (Test-Path $LocalProfile) {
     . $LocalProfile
 }
 
+# Set my preferred prompt
 function prompt {
+$u = [Environment]::UserName
+$m = [Environment]::MachineName
+$d = Get-Location | % Path
+$n = (Get-History -Count 1 | % Id) + 1
+$j = Get-Job | Measure-Object | % Count
 @"
 ___________________________________________________________________________________________________
-$env:USERNAME@$env:COMPUTERNAME  $(gl)  #$((ghy -Count 1 | % Id) + 1) ($(gjb | measure | % Count) bg)
+$u@$m  $d  #$n ($j bg)
 > 
 "@
 }
