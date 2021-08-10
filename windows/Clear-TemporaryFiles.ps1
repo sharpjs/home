7bb45507c6b114874096fb142089324102432951
Jeff Sharp <public@sharpjs.net>
2021-08-09 17:01:06 -0700

    Add Clear-TemporaryFiles script.

---
+++ windows/Clear-TemporaryFiles.ps1
@@ -0,0 +1,23 @@
+#Requires -RunAsAdministrator
+
+[CmdletBinding()]
+param(
+    [Parameter(Position=0)]
+    [ValidateRange(0, 365)]
+    [int]
+    $DaysBack = 3
+)
+
+$CutoffDate = [DateTime]::UtcNow.AddDays(-$DaysBack)
+$Locations  = Get-Item Temp:\, (Join-Path $env:SystemRoot Temp)
+
+$Locations `
+    | Get-ChildItem -Recurse -File `
+    | Where-Object LastWriteTimeUtc -LT $CutoffDate `
+    | Remove-Item -Force -ErrorAction SilentlyContinue -Verbose
+    
+$Locations `
+    | Get-ChildItem -Recurse -Directory `
+    | Sort-Object FullName -Descending `
+    | Where-Object { $_.GetFileSystemInfos().Length -eq 0 } `
+    | Remove-Item -Force -ErrorAction SilentlyContinue -Verbose
